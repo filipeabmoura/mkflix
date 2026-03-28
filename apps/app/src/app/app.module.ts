1b9a3c7313e036982a38472f50497fd0340f2e35
filipemoura <filipemouradev@gmail.com>
2026-03-28 14:49:07 -0300

    feat(app): grids assistidos e favoritos com devextreme

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -1,10 +1,40 @@
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { DxDataGridModule } from "devextreme-angular";
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { AuthInterceptor } from "./core/interceptors/auth.interceptor";
+import { MainShellComponent } from "./layout/main-shell/main-shell.component";
+import { CadastroComponent } from "./pages/cadastro/cadastro.component";
+import { FavoritosComponent } from "./pages/favoritos/favoritos.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { AssistidosComponent } from "./pages/assistidos/assistidos.component";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    MainShellComponent,
+    LoginComponent,
+    CadastroComponent,
+    HomeComponent,
+    AssistidosComponent,
+    FavoritosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    SharedModule,
+    DxDataGridModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ apps/app/src/app/pages/assistidos/assistidos.component.ts
@@ -0,0 +1,35 @@
+import { HttpErrorResponse } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import type { IUsuarioFilmeMarcacao } from "@mk/model";
+import { mensagemErroHttp } from "../../core/api-error";
+import { UsuarioFilmesService } from "../../core/services/usuario-filmes.service";
+import type { ILinhaFilmeGrid } from "../../shared/models/linha-filme-grid";
+
+@Component({
+  selector: "app-assistidos",
+  templateUrl: "./assistidos.component.html",
+  styleUrls: ["./assistidos.component.css"]
+})
+export class AssistidosComponent implements OnInit {
+  linhas: ILinhaFilmeGrid[] = [];
+
+  erro: string | null = null;
+
+  constructor(private readonly filmes: UsuarioFilmesService) {}
+
+  ngOnInit(): void {
+    this.filmes.listarAssistidos().subscribe({
+      next: (marcacoes: IUsuarioFilmeMarcacao[]) => {
+        this.linhas = marcacoes.map((m) => ({
+          titulo: m.filme.titulo,
+          ano: m.filme.ano,
+          imdbId: m.filme.imdbId,
+          marcadoEm: m.marcadoEm
+        }));
+      },
+      error: (e: HttpErrorResponse) => {
+        this.erro = mensagemErroHttp(e);
+      }
+    });
+  }
+}
